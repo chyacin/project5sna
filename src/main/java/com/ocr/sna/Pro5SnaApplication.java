fa9baa4aaaf390f3c6cb6bd6a964e210cb0c4e79
Calvin Hyacinth <46787710+chyacin@users.noreply.github.com>
2020-05-12 12:02:48 +0200

    Spring Initializr Generated Project

---
+++ src/main/java/com/ocr/sna/Pro5SnaApplication.java
@@ -0,0 +1,13 @@
+package com.ocr.sna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pro5SnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pro5SnaApplication.class, args);
+	}
+
+}
