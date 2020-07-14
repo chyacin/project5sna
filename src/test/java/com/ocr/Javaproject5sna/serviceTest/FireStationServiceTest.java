3985751c231062701236e75debadc3d99e1a8afb
calvin <calvin@192.168.0.20>
2020-07-14 17:25:58 +0200

    Service Test Class

--- src/test/java/com/ocr/Javaproject5sna/serviceTest/FireStationServiceTest.java
+++ src/test/java/com/ocr/Javaproject5sna/serviceTest/FireStationServiceTest.java
@@ -1,6 +1,6 @@
 package com.ocr.Javaproject5sna.serviceTest;
 
-import org.json.simple.JSONObject;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -8,10 +8,17 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import com.ocr.Javaproject5sna.dto.EveryHouseHoldInfoDTO;
+import com.ocr.Javaproject5sna.dto.NumberOfPersonsDTO;
+import com.ocr.Javaproject5sna.dto.PersonDetailsDTO;
+import com.ocr.Javaproject5sna.dto.PersonDetailsFromStationNumberDTO;
+import com.ocr.Javaproject5sna.dto.PersonInEachAddressDTO;
+import com.ocr.Javaproject5sna.dto.PersonInfoPlusAddressFromEachStationDTO;
 import com.ocr.Javaproject5sna.model.FireStation;
 import com.ocr.Javaproject5sna.model.MedicalRecord;
 import com.ocr.Javaproject5sna.model.Person;
 import com.ocr.Javaproject5sna.repository.FireStationRepository;
+import com.ocr.Javaproject5sna.repository.PersonRepository;
 import com.ocr.Javaproject5sna.service.FireStationService;
 
 import static org.junit.Assert.assertEquals;
@@ -23,94 +30,88 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-
-
 @RunWith(MockitoJUnitRunner.class)
 public class FireStationServiceTest {
 
-    @InjectMocks
-    FireStationService fireStationService;
-	
+	@InjectMocks
+	FireStationService fireStationService;
+
 	@Mock
 	FireStationRepository fireStationRepository;
-	
-	
+
 	@Test
 	public void createFSAddressMapping_returnFSAddressMapping() {
-		
-		//Arrange
+
+		// Arrange
 		FireStation fireStation = new FireStation();
 		String stationNumber = "2";
 		String address = "188 Culver St";
 		fireStation.addAddress(address);
 		fireStation.setStationNumber(stationNumber);
 		Mockito.when(fireStationRepository.findStation(stationNumber)).thenReturn(fireStation);
-		
-		//Act
+
+		// Act
 		FireStation result = fireStationService.createFSAddressmapping("2", "188 Culver St");
-		 
-		//Assert
-		assertNotNull(result);	
+
+		// Assert
+		assertNotNull(result);
 		assertTrue(result.equals(fireStation));
 		assertEquals(address, new ArrayList(result.getAddresses()).get(0));
 		assertEquals(stationNumber, result.getStationNumber());
-		//System.out.println(fireStation);
-		
+		// System.out.println(fireStation);
+
 	}
-	
+
 	@Test
 	public void updateFSAddressMapping_returnFSAddressMapping() {
-		
-		//Arrange
+
+		// Arrange
 		FireStation fireStation = new FireStation();
 		String stationNumber = "2";
 		String address = "188 Culver St";
 		fireStation.addAddress(address);
 		fireStation.setStationNumber(stationNumber);
 		List<FireStation> fireStations = new ArrayList<>();
-	    fireStations.add(fireStation);
+		fireStations.add(fireStation);
 		Mockito.when(fireStationRepository.createStation(Mockito.any(FireStation.class))).thenReturn(fireStation);
-	    Mockito.when(fireStationRepository.findAll()).thenReturn(fireStations);
-		
-		//Act
+		Mockito.when(fireStationRepository.findAll()).thenReturn(fireStations);
+
+		// Act
 		fireStationService.updateAddressFSNumber("3", "175 Culver St");
-		
-		//Assert 
+
+		// Assert
 		Mockito.verify(fireStationRepository).addFireStation(fireStation);
-		
+
 	}
-		
-	
-	
+
 	@Test
-	public void deleteFSAddressMapping_successfullyDeletedFSAddressMapping() {	
-		
-		//Act
+	public void deleteFSAddressMapping_successfullyDeletedFSAddressMapping() {
+
+		// Act
 		fireStationService.deleteFSAddressMapping("2", "175 Culver St");
-		
-		//Assert
+
+		// Assert
 		Mockito.verify(fireStationRepository, times(1));
 	}
-	
-	
+
 	// url end points testing
-	
+
 	@Test
 	public void getPersonDetailsFromFireStationNumber() {
-		
-	  //Arrange
+
+		// Arrange
 		MedicalRecord medicalRecord = new MedicalRecord();
 		medicalRecord.setBirthDate("03/06/1984");
 		MedicalRecord medicalRecord1 = new MedicalRecord();
 		medicalRecord1.setBirthDate("09/06/2017");
-		
+
 		Person person = new Person();
 		person.setFirstName("John");
 		person.setLastName("Boyd");
 		person.setAddress("1509 Culver St");
 		person.setPhone("841-874-6512");
 		person.setMedicalRecord(medicalRecord);
-		
+
 		Person person1 = new Person();
 		person1.setFirstName("Roger");
 		person1.setLastName("Boyd");
@@ -118,148 +119,149 @@ public class FireStationServiceTest {
 		person1.setPhone("841-874-6512");
 		person1.setMedicalRecord(medicalRecord1);
 
-				
 		String stationNumber = "3";
-		
+
 		FireStation fireStation = new FireStation(stationNumber);
 		fireStation.addPerson(person);
-		fireStation.addPerson(person1);	
-		
+		fireStation.addPerson(person1);
+
 		List<FireStation> fireStations = new ArrayList<>();
 		fireStations.add(fireStation);
 		Mockito.when(fireStationRepository.findAll()).thenReturn(fireStations);
-		
-		//Act
-		JSONObject result = fireStationService.getPersonDetailsFromFireStationNumber(stationNumber);
-		List<JSONObject> numberOfKidsPlusAdults = (List<JSONObject>) result.get("numberOfKidsPlusAdults");
-		List<JSONObject> personsDetails = (List<JSONObject>) result.get("personsDetails");
-		
-		//Assert
-		assertNotNull(result);	
+
+		// Act
+		PersonDetailsFromStationNumberDTO result = fireStationService
+				.getPersonDetailsFromFireStationNumber(stationNumber);
+		ArrayList<NumberOfPersonsDTO> numberOfKidsPlusAdults = result.getNumberOfKidsPlusAdult();
+		ArrayList<PersonDetailsDTO> personsDetails = result.getPersonsDetails();
+
+		// Assert
+		assertNotNull(result);
 		assertEquals(1, numberOfKidsPlusAdults.size());
 		assertEquals(2, personsDetails.size());
-		assertEquals(2,result.size());			
-		
+		// System.out.println(personsDetails);
+
 	}
+
 	@Test
 	public void getPersonPhoneNumberFromWithInEachFireStation() {
-		
+
 		Person person = new Person();
 		person.setFirstName("John");
 		person.setLastName("Boyd");
 		person.setAddress("1509 Culver St");
 		person.setPhone("841-874-6512");
-		
+
 		Person person1 = new Person();
 		person1.setFirstName("Jacob");
 		person1.setLastName("Boyd");
 		person1.setAddress("1509 Culver St");
 		person1.setPhone("841-874-6513");
-		
+
 		String stationNumber = "3";
 		FireStation fireStation = new FireStation(stationNumber);
 		fireStation.addPerson(person);
 		fireStation.addPerson(person1);
-		
+
 		List<FireStation> fireStations = new ArrayList<>();
 		fireStations.add(fireStation);
-		
+
 		Mockito.when(fireStationRepository.findAll()).thenReturn(fireStations);
-		
-		//Act
-		List<String> result = fireStationService.getPersonPhoneNumberFromWithInEachFireStation("3");
-		
-		//Assert
+
+		// Act
+		List<String> result = fireStationService.getPersonPhoneNumberFromFireStationNumber("3");
+
+		// Assert
 		assertNotNull(result);
 		assertEquals(2, result.size());
-     	assertEquals("841-874-6512", result.get(0));
-     	assertEquals("841-874-6513", result.get(1));
-        //System.out.println(result.get(0));
-        //System.out.println(result.get(1));
-	} 
-	
+		assertEquals("841-874-6512", result.get(0));
+		assertEquals("841-874-6513", result.get(1));
+		// System.out.println(result.get(0));
+		// System.out.println(result.get(1));
+	}
+
 	@Test
 	public void getAddressFromEachStation() {
-		
-		//Arrange
+
+		// Arrange
 		Set<String> medications = new HashSet<String>();
 		medications.add("aznol:350mg, hydrapermazol:100mg");
-	    Set<String> allergies = new HashSet<>();
+		Set<String> allergies = new HashSet<>();
 		allergies.add("nilliacilan");
 		MedicalRecord medicalRecord = new MedicalRecord();
 		medicalRecord.setBirthDate("03/06/1984");
 		medicalRecord.setMedications(medications);
 		medicalRecord.setAllergies(allergies);
-		
+
 		Person person = new Person();
 		person.setFirstName("John");
 		person.setLastName("Boyd");
 		person.setAddress("1509 Culver St");
 		person.setPhone("841-874-6512");
 		person.setMedicalRecord(medicalRecord);
-		
-		String stationNumber ="3";
+
+		String stationNumber = "3";
 		String address = "1509 Culver St";
-		
+
 		FireStation fireStation = new FireStation(stationNumber, address);
 		fireStation.addPerson(person);
-		
+
 		List<FireStation> fireStations = new ArrayList<>();
 		fireStations.add(fireStation);
 		Mockito.when(fireStationRepository.findAll()).thenReturn(fireStations);
 
-		//Act
-		JSONObject result = fireStationService.getAddressFromEachStation(address);
-		List<JSONObject> personsInAddress = (List<JSONObject>) result.get("personsInAddress");
-		String fireStationNumber = (String) result.get("fireStationNumber");
-		
-		//Assert
-	   assertNotNull(result);	
-       assertEquals(1, personsInAddress.size());
-       assertEquals(1, stationNumber.length());
-       assertEquals(2, result.size());
-       assertEquals(address, personsInAddress.get(0).get("address"));
-      // assertEquals(address, fireStationNumber);
+		// Act
+		PersonInfoPlusAddressFromEachStationDTO result = fireStationService.getAddressFromEachStation(address);
+		ArrayList<PersonInEachAddressDTO> personsInAddress = result.getPersonsInAddress();
 
-     //  System.out.println(personsInAddress);
+		// Assert
+		assertNotNull(result);
+		assertEquals(1, personsInAddress.size());
+		assertEquals(1, stationNumber.length());
+		assertEquals(address, personsInAddress.get(0).getAddress());
+
+		// System.out.println(personsInAddress);
 	}
-	
+
 	@Test
 	public void getPersonByHouseHoldsInEachStationNumber() {
-		
-		//Arrange
+
+		// Arrange
 		Set<String> medications = new HashSet<String>();
 		medications.add("aznol:350mg, hydrapermazol:100mg");
-	    Set<String> allergies = new HashSet<>();
+		Set<String> allergies = new HashSet<>();
 		allergies.add("nilliacilan");
 		MedicalRecord medicalRecord = new MedicalRecord();
 		medicalRecord.setBirthDate("03/06/1984");
 		medicalRecord.setMedications(medications);
 		medicalRecord.setAllergies(allergies);
-				
+
 		Person person = new Person();
 		person.setFirstName("John");
 		person.setLastName("Boyd");
-		person.setAddress("1509 Culver St");
 		person.setPhone("841-874-6512");
+		person.setAddress("188 Culver St");
 		person.setMedicalRecord(medicalRecord);
+
+		PersonRepository personRepository = new PersonRepository();
+		personRepository.createPerson(person);
+		String stationNumber = "3";
 		
-		
-		String stationNumber ="3";
+
 		FireStation fireStation = new FireStation(stationNumber);
 		fireStation.addPerson(person);
-		
+
 		List<FireStation> fireStations = new ArrayList<>();
 		fireStations.add(fireStation);
 		Mockito.when(fireStationRepository.findAll()).thenReturn(fireStations);
-		
-		//Act
-		List<JSONObject> result = fireStationService.getPersonByHouseHoldsInEachStationNumber(stationNumber);
-		
-		
-		//Assert
-		assertNotNull(result); 
-		//System.out.println(result);
-		
+		fireStationRepository.createStation(fireStation);
+
+		// Act
+		List<EveryHouseHoldInfoDTO> result = fireStationService.getPersonByHouseHoldsInEachStationNumber(stationNumber);
+
+		// Assert
+		assertNotNull(result);
+		System.out.println(result);
+
 	}
 }
--- src/test/java/com/ocr/Javaproject5sna/serviceTest/PersonServiceTest.java
+++ src/test/java/com/ocr/Javaproject5sna/serviceTest/PersonServiceTest.java
@@ -1,6 +1,5 @@
 package com.ocr.Javaproject5sna.serviceTest;
 
-import org.json.simple.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -8,7 +7,9 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
-
+import com.ocr.Javaproject5sna.dto.ChildAlertDTO;
+import com.ocr.Javaproject5sna.dto.PersonNamePlusAgeDTO;
+import com.ocr.Javaproject5sna.dto.PersonMedicalInfoDTO;
 import com.ocr.Javaproject5sna.model.MedicalRecord;
 import com.ocr.Javaproject5sna.model.Person;
 import com.ocr.Javaproject5sna.repository.PersonRepository;
@@ -18,26 +19,25 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
-
+import static org.mockito.Mockito.times;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-
 @RunWith(MockitoJUnitRunner.class)
 public class PersonServiceTest {
-	
+
 	@InjectMocks
 	PersonService personService;
-	
+
 	@Mock
 	PersonRepository personRepository;
-	
+
 	@Test
 	public void createPerson_returnCreatedPerson() {
-		
+
 		Person person = new Person();
 		person.setFirstName("John");
 		person.setLastName("Boyd");
@@ -47,11 +47,11 @@ public class PersonServiceTest {
 		person.setPhone("888-987-7878");
 		person.setEmail("jaboyd@email.com");
 		when(personRepository.createPerson(person)).thenReturn(person);
-		
-		//Act
+
+		// Act
 		Person createPerson = personService.createPerson(person);
-		
-		//Assert
+
+		// Assert
 		assertEquals(createPerson.getFirstName(), "John");
 		assertEquals(createPerson.getLastName(), "Boyd");
 		assertEquals(createPerson.getAddress(), "175 Culver St");
@@ -59,14 +59,12 @@ public class PersonServiceTest {
 		assertEquals(createPerson.getZip(), "75009");
 		assertEquals(createPerson.getPhone(), "888-987-7878");
 		assertEquals(createPerson.getEmail(), "jaboyd@email.com");
-		
+
 	}
-	
+
 	@Test
-	public void getAllPerson_returnAllPerson() {
-		
-		List<Person> person = new ArrayList<>();
-		
+	public void updatePerson_returnUpdatedPerson() {
+
 		Person person1 = new Person();
 		person1.setFirstName("John");
 		person1.setLastName("Boyd");
@@ -75,129 +73,124 @@ public class PersonServiceTest {
 		person1.setZip("97451");
 		person1.setPhone("841-874-7466");
 		person1.setEmail("encore317@email.com");
-		
-        Person person2 = new Person();
-		person2.setFirstName("David");
-		person2.setLastName("Bower");
-		person2.setAddress("144 Culver St");
-		person2.setCity("Culver");
-		person2.setZip("97451");
-		person2.setPhone("845-875-7444");
-		person2.setEmail("encore@email.com");
-		
-		person.add(person1);
-		person.add(person2);
-		when(personRepository.findAll()).thenReturn(person);
-		
-		//Act		
-		List<Person> result = personService.getAllPerson();
-		
-		//Assert
-		assertEquals(2, result.size());
-		assertTrue(result.equals(person));
-		
+
+		when(personRepository.findPerson("John", "Boyd")).thenReturn(person1);
+
+		// Act
+		personService.updatePerson(person1);
+
+		// Assert
+		Mockito.verify(personRepository, times(1));
+
 	}
-		
+
 	@Test
-	public void jsonObject_getPersonInfo_returnPersonInfo() {
-		
-		//Arrange
+	public void deletePerson() {
+
+		personService.deletePerson("John", "Boyd");
+
+		Mockito.verify(personRepository, times(1));
+	}
+
+	@Test
+	public void getPersonInfo_returnPersonInfo() {
+
+		// Arrange
 		Set<String> medications = new HashSet<String>();
 		medications.add("aznol:350mg, hydrapermazol:100mg");
-	    Set<String> allergies = new HashSet<>();
+		Set<String> allergies = new HashSet<>();
 		allergies.add("nilliacilan");
 		MedicalRecord medicalRecord = new MedicalRecord();
 		medicalRecord.setBirthDate("01/07/1998");
 		medicalRecord.setMedications(medications);
 		medicalRecord.setAllergies(allergies);
-		
+
 		String firstName = "John";
 		String lastName = "Boyd";
-		Person person = new Person(firstName, lastName, "888-545-5656", "5400", "1509 Culver St", "Culver", "jaboyd@email.com");
+		Person person = new Person(firstName, lastName, "888-545-5656", "5400", "1509 Culver St", "Culver",
+				"jaboyd@email.com");
 		person.setMedicalRecord(medicalRecord);
 		Person person1 = new Person("Soh", "Dod", "1509 Culver", "gaboyd@email", "888-345-345", "Sulver", "5400");
-		
+
 		List<Person> persons = new ArrayList<>();
 		persons.add(person);
 		persons.add(person1);
-			
+
 		Mockito.when(personRepository.findAll()).thenReturn(persons);
-			 
-		//Act
-        List<JSONObject> result = personService.getPersonInfo(firstName, lastName);
-		 
-		//Assert
-    	assertNotNull(result);
-    	System.out.println(result);
+
+		// Act
+		List<PersonMedicalInfoDTO> result = personService.getPersonInfo(firstName, lastName);
+
+		// Assert
+		assertNotNull(result);
+		System.out.println(result);
 		assertEquals(1, result.size());
-		//extracting the json using a key
-		assertEquals(firstName, result.get(0).get("firstName"));
-		assertEquals(lastName, result.get(0).get("lastName"));
-		
-			 	
+		// extracting the json using a key
+		assertEquals(firstName, result.get(0).getFirstName());
+		assertEquals(lastName, result.get(0).getLastName());
+
 	}
-		
+
 	@Test
 	public void getPersonsEmail_returnPersonsEmail() {
-		
-		//Arrange
+
+		// Arrange
 		String city = "Culver";
 		String email = "jaboyd@email.com";
-		Person person = new Person("John", "Boyd", "888-545-5656", "5400", "1509 Culver St", city, email);	
+		Person person = new Person("John", "Boyd", "888-545-5656", "5400", "1509 Culver St", city, email);
 		Person person1 = new Person("John", "Boyd", "1509 Culver", "gaboyd@email.com", "888-345-345", "Sulver", "5400");
 		List<Person> persons = new ArrayList<>();
 		persons.add(person);
 		persons.add(person1);
 		Mockito.when(personRepository.findAll()).thenReturn(persons);
-		
-		//Act
+
+		// Act
 		List<String> result = personService.getPersonsEmailAddress("Culver");
-		
-		//Assert
+
+		// Assert
 		assertNotNull(result);
 		assertEquals(1, result.size());
 		assertEquals(email, result.get(0));
-		
-		
+
 	}
-	
+
 	@Test
 	public void jsonObject_getChildrenFromEachAddress_returnChildrenFromEachAddress() {
-		
-		//Arrange
+
+		// Arrange
 		MedicalRecord medicalRecord = new MedicalRecord();
 		medicalRecord.setBirthDate("03/06/1984");
 		MedicalRecord medicalRecord1 = new MedicalRecord();
 		medicalRecord1.setBirthDate("09/06/2017");
-			
+
 		String address = "1509 Culver St";
-		
-		Person person = new Person("John", "Boyd", "888-545-5656", "5400", address, "Culver", "jaboyd@email.com");	
+
+		Person person = new Person("John", "Boyd", "888-545-5656", "5400", address, "Culver", "jaboyd@email.com");
 		person.setMedicalRecord(medicalRecord);
 		Person person1 = new Person("Roger", "Boyd", "888-545-5656", "5400", address, "Culver", "jaboyd@email.com");
 		person1.setMedicalRecord(medicalRecord1);
-        Person person2 = new Person("Sonny", "Boy", "888-545-5656", "5400", "17 Culver St", "Culver", "jaboyd@email.com");
-		
-        
+		Person person2 = new Person("Sonny", "Boy", "888-545-5656", "5400", "17 Culver St", "Culver",
+				"jaboyd@email.com");
+
 		List<Person> persons = new ArrayList<>();
 		persons.add(person);
 		persons.add(person1);
 		persons.add(person2);
 		Mockito.when(personRepository.findAll()).thenReturn(persons);
 
-		//Act
-		JSONObject result = personService.getChildrenFromEachAddress(address);
-		List<JSONObject> children = (List<JSONObject>) result.get("children");
-		List<JSONObject> adults = (List<JSONObject>) result.get("adults");
-		
-		//Assert
+		// Act
+		ChildAlertDTO result = personService.getChildrenFromEachAddress(address);
+		List<PersonNamePlusAgeDTO> children = result.getChildren();
+		List<PersonNamePlusAgeDTO> adults = result.getAdults();
+
+		// Assert
 		assertNotNull(result);
 		assertEquals(1, children.size());
-		assertEquals(1, adults.size()); 
-		assertEquals(2, result.size());
-		assertEquals(address, children.get(0).get("address"));
-		assertEquals(address, adults.get(0).get("address"));
-		
+		assertEquals(1, adults.size());
+		assertEquals("John", adults.get(0).getFirstName());
+		assertEquals("Boyd", adults.get(0).getLastName());
+		assertEquals("Roger", children.get(0).getFirstName());
+		assertEquals("Boyd", children.get(0).getLastName());
 
 	}
 
