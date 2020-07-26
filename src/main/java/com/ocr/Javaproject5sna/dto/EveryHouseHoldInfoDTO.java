6aa92f1bb9ed09c919189385688761d9eac27d43
chyacin <calvinhyacinth@gmail.com>
2020-07-26 09:42:45 +0200

    Minor changes made

--- src/main/java/com/ocr/Javaproject5sna/dto/EveryHouseHoldInfoDTO.java
+++ src/main/java/com/ocr/Javaproject5sna/dto/EveryHouseHoldInfoDTO.java
@@ -1,10 +1,13 @@
 package com.ocr.Javaproject5sna.dto;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class EveryHouseHoldInfoDTO {
 
-	private String address;
+	private Set<String> addresses = new HashSet<>();
 	private ArrayList<PersonInEachAddressDTO> everybodyInfo;
 
 	public EveryHouseHoldInfoDTO() {
@@ -12,7 +15,7 @@ public class EveryHouseHoldInfoDTO {
 	}
 
 	public EveryHouseHoldInfoDTO(String address, ArrayList<PersonInEachAddressDTO> everybodyInfo ) {
-        this.address = address;
+		this.addresses.add(address);
         this.everybodyInfo = everybodyInfo;
 
 		
@@ -26,11 +29,31 @@ public class EveryHouseHoldInfoDTO {
 		this.everybodyInfo = everybodyInfo;
 	}
 	
-	public String getAddress() {
-		return address;
+	public EveryHouseHoldInfoDTO addAddress (String address) {
+		addresses.add(address);
+		return this;
 	}
 	
-	public void setAddress(String address) {
-		this.address = address;
+	public String getAddress() {
+		return this.getAddresses().toString();
 	}
+
+	public Set<String> getAddresses() {
+		return addresses.stream().collect(Collectors.toSet());
+	}
+
+	public void setAddresses(Set<String> addresses) {
+		this.addresses = addresses;
+	}
+
+	
 }
+//	
+//	public String getAddress() {
+//		return address;
+//	}
+//	
+//	public void setAddress(String address) {
+//		this.address = address;
+//	}
+//}
